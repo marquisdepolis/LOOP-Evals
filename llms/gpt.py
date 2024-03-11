87f6b688a859edd1d8525cbc59d61115932d2372
Rohit <rohit.krishnan@gmail.com>
2024-03-11 16:10:41 +0000

    Update gpt.py

--- llms/gpt.py
+++ llms/gpt.py
@@ -20,3 +20,19 @@ def llm_call(input, GPT):
         # response_format={ "type": "json_object" }
     )
     return response.choices[0].message.content
+
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
+@retry_except(exceptions_to_catch=(IndexError, ZeroDivisionError), tries=3, delay=2)
+def llm_call_json(input, GPT):
+    client = OpenAI()
+    client.api_key = os.getenv('OPENAI_API_KEY')
+
+    response = client.chat.completions.create(
+        model=GPT,
+        messages=[
+            {"role": "system", "content": """You are an AI designed to solve word puzzles. You are brilliant and clever."""},
+            {"role": "user", "content": f"{input}"}
+        ],
+        response_format={ "type": "json_object" }
+    )
+    return response.choices[0].message.content
