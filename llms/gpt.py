b948c55592355434e35778a86ce1bff4ef24a25e
Rohit <rohit.krishnan@gmail.com>
2024-03-12 14:58:05 +0000

    Adding Claude Opus

--- llms/gpt.py
+++ llms/gpt.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+from anthropic import Anthropic
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -16,8 +17,7 @@ def llm_call(input, GPT):
         messages=[
             {"role": "system", "content": """You are an AI designed to solve word puzzles. You are brilliant and clever."""},
             {"role": "user", "content": f"{input}"}
-        ],
-        # response_format={ "type": "json_object" }
+        ]
     )
     return response.choices[0].message.content
 
@@ -36,3 +36,17 @@ def llm_call_json(input, GPT):
         response_format={ "type": "json_object" }
     )
     return response.choices[0].message.content
+
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
+@retry_except(exceptions_to_catch=(IndexError, ZeroDivisionError), tries=3, delay=2)
+def llm_call_claude(input, LLM):
+    client = Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
+
+    response = client.messages.create(
+        model=LLM,
+        messages=[
+            {"role": "system", "content": """You are an AI designed to solve word puzzles. You are brilliant and clever."""},
+            {"role": "user", "content": f"{input}"}
+        ]
+    )
+    return response.content[0].text
